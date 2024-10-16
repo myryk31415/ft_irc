d45ce02543adad6124964c6468b5d724fb4d31dc
Myryk <myryk@posteo.de>
2024-10-16 22:13:20 +0200

    server basic init and signal handler added

--- include/Server.hpp
+++ include/Server.hpp
@@ -3,30 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   Server.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aweizman <aweizman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: padam <padam@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 15:38:31 by padam             #+#    #+#             */
-/*   Updated: 2024/10/16 16:30:22 by aweizman         ###   ########.fr       */
+/*   Updated: 2024/10/16 21:44:41 by padam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 # include <vector>
+# include <poll.h>
+# include <csignal>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <netinet/ip.h>
+# include "ft_irc.hpp"
 # include "Client.hpp"
 # include "Channel.hpp"
-# include <poll.h>
-# include <signal.h>
-# include <sys/socket.h>
+
+struct sockaddr_in {
+    sa_family_t    sin_family; /* address family: AF_INET */
+    in_port_t      sin_port;   /* port in network byte order */
+    struct in_addr sin_addr;   /* internet address */
+};
+
 class Server
 {
 	private:
 		static bool _Signal;
 		int _port;
-		int _server_sock_fd;
+		int _server_socket_fd;
 		std::vector<struct pollfd> _socks;
 		std::vector<Client> _clients;
 	public:
-		Server();
+		Server() = delete;
+		Server(int port);
 		~Server();
+
+		static void	signal_handler(int signal);
+
+		void		init();
+		void		create_socket();
 };
--- src/Server.cpp
+++ src/Server.cpp
@@ -3,19 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   Server.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: padam <padam@student.42heilbronn.com>      +#+  +:+       +#+        */
+/*   By: padam <padam@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 15:38:40 by padam             #+#    #+#             */
-/*   Updated: 2024/10/16 16:15:13 by padam            ###   ########.fr       */
+/*   Updated: 2024/10/16 22:12:54 by padam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 
-Server::Server()
+Server::Server(int port) : _port(port) {}
+
+Server::~Server() {}
+
+void	Server::signal_handler(int signal)
 {
+	std::cout << std::endl << "Signal " << signal << " received!" << std::endl;
 }
 
-Server::~Server()
+void	Server::init()
 {
+	// TODO throw errors?
+	if (signal(SIGINT, signal_handler) == SIG_ERR) // "ctrl + c"
+		return;
+	if (signal(SIGQUIT, signal_handler) == SIG_ERR); // "ctrl + \"
+		return;
+
+	_server_socket_fd = socket(AF_INET, SOCK_STREAM, 0); // create IPv4 TCP socket
+	if (_server_socket_fd == -1)
+		return;
+
+	sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = _port;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	if (bind(_server_socket_fd, (struct sockaddr *)&addr, sizeof(sockaddr_in)) == -1)
+		return;
+	// maybe setsockopt and fnctl
+	if (listen(_server_socket_fd, 10) == -1) // wait for connections, 10 is arbitrary
+		return;
+
+	//polling the socket needs to be done
+
 }
