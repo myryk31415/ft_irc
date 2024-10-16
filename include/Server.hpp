f139c974c1e02d56dbd8628c0c9bf870d3f673b7
antonweizmann <anton@weizmann.de>
2024-10-16 16:37:41 +0200

    Server class started

--- include/Server.hpp
+++ include/Server.hpp
@@ -1,21 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Server.h                                           :+:      :+:    :+:   */
+/*   Server.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: padam <padam@student.42heilbronn.com>      +#+  +:+       +#+        */
+/*   By: aweizman <aweizman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 15:38:31 by padam             #+#    #+#             */
-/*   Updated: 2024/10/16 15:38:32 by padam            ###   ########.fr       */
+/*   Updated: 2024/10/16 16:30:22 by aweizman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+# include <vector>
+# include "Client.hpp"
+# include "Channel.hpp"
+# include <poll.h>
+# include <signal.h>
+# include <sys/socket.h>
 class Server
 {
 	private:
-		/* data */
+		static bool _Signal;
+		int _port;
+		int _server_sock_fd;
+		std::vector<struct pollfd> _socks;
+		std::vector<Client> _clients;
 	public:
 		Server();
 		~Server();
