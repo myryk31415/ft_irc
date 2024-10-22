ac4ae29acb431dea9d95637f74f8748698bf1bb1
antonweizmann <aweizman@student.42heilbronn.de>
2024-10-22 12:55:53 +0200

    can now talk with server

--- src/Server.cpp
+++ src/Server.cpp
@@ -160,7 +160,11 @@ void	Server::receiveData(int fd)
 		std::cout << MAGENTA << "Client " << fd << " data: " << RESET << buff << std::endl;
 		splitData(buff, cmd);
 		if (cmd[0].compare("CAP LS 302"))
-			sendResponse("CAP * LS : ", fd);
+			sendResponse("CAP * LS :\r\n", fd);
+		if (cmd[0].compare("CAP REQ :"))
+			sendResponse("CAP * ACK :\r\n", fd);
+		if (cmd[0].compare("CAP END"))
+			sendResponse(": 001 MONI DIGGA", fd);
 		for (auto it = cmd.begin(); it != cmd.end(); it++)
 			parseCommand(*it);
 	}
